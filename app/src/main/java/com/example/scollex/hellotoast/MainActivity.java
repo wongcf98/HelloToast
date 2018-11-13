7277b07526bb0c03a33d246bdd378b574994726e
wongcf98 <44184737+wongcf98@users.noreply.github.com>
2018-11-13 19:49:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/scollex/hellotoast/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.scollex.hellotoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+
+    }
+}
