98abce1d180a11734aced87781ecb099b61876e8
wongcf98 <44184737+wongcf98@users.noreply.github.com>
2018-11-14 22:20:11 +0800

    up to relativeLayout 01.2 part b

--- app/src/main/java/com/example/scollex/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/scollex/hellotoast/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.scollex.hellotoast;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int mCount = 0;
+    private TextView mShowCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+
     }
 
     public void showToast(View view) {
@@ -20,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void countUp(View view) {
-
+        mCount++;
+        if(mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
